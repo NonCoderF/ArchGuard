cf5ffcf8514ec04add2a654bf0298136470143e5
Jimmy <sallyinfo365@gmail.com>
2026-07-13 22:15:45 +0530

    Release v0.1.2

--- archguard-gradle-plugin/build.gradle.kts
+++ archguard-gradle-plugin/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 
 group = "io.github.noncoderf.archguard"
-version = "0.1.10"
+version = "0.1.2"
 
 java {
     toolchain {
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "io.github.noncoderf.archguard"
-version = "0.1.10"
+version = "0.1.2"
 
 allprojects {
     repositories {
